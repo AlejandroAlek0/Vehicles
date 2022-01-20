ec62b086e796bba0a28071ebd9b12e3853ce66ed
Alejandro Nuñez Maldonado <aleko.nunez.maldonado@gmail.com>
2022-01-20 15:16:37 +0100

    init

---
+++ Vehicle.API/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Vehicle.API.Data.Entities;
+
+namespace Vehicle.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> option) : base(option)
+        {
+
+        }
+
+        public DbSet<VehicleType> VehicleTypes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<VehicleType>().HasIndex(x => x.Description).IsUnique();
+        }
+    }
+}
